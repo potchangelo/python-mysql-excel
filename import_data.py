40cc5c0cfb317514d1b8cd10030e9ae4a0fa29e0
Potchara Puttawanchai <potchangelo@gmail.com>
2021-05-22 13:40:59 +0700

    Add comments

--- import_data.py
+++ import_data.py
@@ -1,16 +1,23 @@
+# Import ข้อมูลทุกแถวจากไฟล์ Excel (.xlsx) เข้าสู่ Database MySQL
+# โดยข้อมูลจากไฟล์ Excel จะเริ่มต้นตรงแถวที่ 2
+
 import mysql.connector
 from openpyxl import load_workbook
 
 # Excel
+# - โหลดไฟล์ และโหลด Sheet ที่เปิดอยู่
 workbook = load_workbook('imported.xlsx')
 sheet = workbook.active
 
+# - เก็บข้อมูล (values_only คือ แบบดิบๆ) ทีละแถวไว้ใน List
+# - เริ่มต้นจากแถวที่ 2 ไปจนถึงแถวสุดท้าย
 values = []
 for row in sheet.iter_rows(min_row=2, values_only=True):
     print(row)
     values.append(row)
 
-# Database (เปลี่ยนค่า Connection เป็นของเครื่องตัวเองเน่อ)
+# Database
+# - เชื่อมต่อ Database (เปลี่ยนค่า Connection เป็นของเครื่องตัวเองเน่อ)
 db = mysql.connector.connect(
     host="localhost",
     port=3306,
@@ -19,6 +26,8 @@ db = mysql.connector.connect(
     database='golf_want_to_buy'
 )
 
+# - ส่งคำสั่ง SQL ไปให้ MySQL ทำการเพิ่มข้อมูล
+# - ใช้ executemany() เพื่อเพิ่มข้อมูลหลายอัน
 cursor = db.cursor()
 sql = '''
     INSERT INTO products (title, price, is_necessary)
@@ -26,4 +35,6 @@ sql = '''
 '''
 cursor.executemany(sql, values)
 db.commit()
-print('เพิ่ม ' + str(cursor.rowcount) + ' ข้อมูล')
+
+# - สรุปจำนวนข้อมูลที่เพิ่มไป
+print('เพิ่มข้อมูลจำนวน ' + str(cursor.rowcount) + ' แถว')
